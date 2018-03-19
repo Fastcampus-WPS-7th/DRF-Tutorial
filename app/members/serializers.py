7162b35dc724d60ed38f4204af5f3fc1d5e16c43
LeeHanYeong <dev@azelf.com>
2018-03-19 14:57:49 +0900

    UserSerializer에 snippets를 HyperlinkedRelatedField로 설정

--- app/members/serializers.py
+++ app/members/serializers.py
@@ -1,15 +1,14 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from snippets.models import Snippet
-
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        queryset=Snippet.objects.all(),
+    snippets = serializers.HyperlinkedRelatedField(
+        view_name='snippets:snippet-detail',
         many=True,
+        read_only=True,
     )
 
     class Meta:
