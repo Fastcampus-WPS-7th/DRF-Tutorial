84bf242d16d440bef582745be43a57961b30ad89
LeeHanYeong <dev@azelf.com>
2018-03-19 14:50:06 +0900

    SnippetHighlight구현

--- app/snippets/urls/generic.py
+++ app/snippets/urls/generic.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from ..apis.generic import SnippetList, SnippetDetail
+from ..apis.generic import SnippetList, SnippetDetail, SnippetHighlight
 
 urlpatterns = [
     path('', SnippetList.as_view(), name='snippet-list'),
     path('<int:pk>/', SnippetDetail.as_view(), name='snippet-detail'),
+    # SnippetHighlight를 문서와 같게 동작하도록 설정
+    path('<int:pk>/highlight/', SnippetHighlight.as_view(), name='snippet-highlight'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
